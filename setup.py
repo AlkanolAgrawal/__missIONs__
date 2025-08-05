1ddb318f4741e2656b80274ed1cc70baef635b85
AlkanolAgrawal <coding.alkanol@gmail.com>
2025-08-05 06:50:58 +0530

    code_improvs

--- setup.py
+++ setup.py
@@ -1,35 +1,23 @@
 from setuptools import setup, find_packages
 
+def get_requireds(file_path):
+    with open(file_path) as f:
+        lst = f.readlines()
+        lstn=[]
+        for x in lst:
+            a=x.strip()
+            if a == "":
+                continue
+            if "-e ." == a:
+                continue
+            else:
+                lstn.append(a)
+    return lstn
 setup(
     name='First_Mission',
     version='0.0.1',
     author='Alkanol',
+    author_email='coding.alkanol@gmail.com',
     packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'pandas',
-        'scikit-learn',
-        'matplotlib',
-        'seaborn',
-        'nltk',
-        'spacy',
-        'scipy',
-        'requests',
-        'beautifulsoup4',
-        'flask',
-        'django',
-        'fastapi',
-        'pytest',
-        'jupyterlab',
-        'notebook',
-        'ipython',
-        'scikit-image',
-        'opencv-python',
-        'plotly',
-        'statsmodels',
-        'xgboost',
-        'lightgbm',
-        'catboost',
-        'pydantic',
-    ],
+    install_requires=get_requireds('requirements.txt'),
 )
